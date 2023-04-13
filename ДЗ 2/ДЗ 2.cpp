79eee629de3dc6526d2434bd5e8c36d4dcab4172
step-_30o8zgr <step-_30o8zgr@DESKTOP-1Q8BNJC>
2023-04-13 18:41:05 +0300

    Добавьте файлы проекта.

---
+++ ДЗ 2/ДЗ 2.cpp
@@ -0,0 +1,47 @@
+﻿//Задание 1
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	cout << "Home task #2.1\n\n";
+//	cout << "Hold fast to dreams\n";
+//	cout << "For if dreams die\n";
+//	cout << "Life is a broken - winged bird\n";
+//	cout << "That cannot fly.\n";
+//	cout << "Hold fast to dreams\n";
+//	cout << "For when dreams go\n";
+//	cout << "Life is a barren field\n";
+//	cout << "Frozen with snow.\n";
+//	return 0;
+//}
+
+//Задание 2
+//#include <iostream>
+//using namespace std;
+//int main()
+//{0
+//	cout << "Home task #2.2\n\n";
+//	cout << "Every" << "\n";
+//	cout << "\tHunter" << "\n";
+//	cout << "\t\tWants" << "\n";
+//	cout << "\t\t\tTo know" << "\n";
+//	cout << "\t\t\t\t Where" << "\n";
+//	cout << "\t\t\t\t\tDoes Pheasant" << "\n";
+//	cout << "\t\t\t\t\t\t\tSit" << "\n\n";
+//	return 0;
+//}
+
+//Задание 3
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	cout << "Home task #2.3\n\n";
+//	cout << "\t\t\t\t\tPuppies for sale" << "\n";
+//	cout << "\t\t\t\t\tDog breed: Rottweiler" << "\n";
+//	cout << "\t\t\t\t\tTel: 222-22-22" << "\n";
+//	cout << "\t\t\t\t\t222\t|22-22|" << "\n";
+//	cout << "\t\t\t\t\t222\t|22-22|" << "\n";
+//	cout << "\t\t\t\t\t222\t|22-22|" << "\n";
+//	return 0;
+//}
